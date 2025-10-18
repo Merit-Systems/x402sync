550fb2133f57a80ef5b250c81765c747761ed234
shafu <shafu0x@gmail.com>
2025-10-18 17:55:07 -0400

    cdp

--- trigger/constants.ts
+++ trigger/constants.ts
@@ -5,3 +5,6 @@ export const TIME_WINDOW_DAYS = 10;
 export const USDC_MULTIPLIER = 1_000_000;
 export const DEFAULT_CONTRACT_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const USDC_SOLANA = "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v";
+
+export const USDC_BASE = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"; // USDC on Base
+export const TRANSFER_EVENT_SIG = "Transfer(address,address,uint256)";
