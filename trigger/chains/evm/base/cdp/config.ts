6fd2ec3ee32b907970978ee144911784855aaf88
shafu <shafu0x@gmail.com>
2025-10-20 11:41:01 -0400

    fmt

--- trigger/chains/evm/base/cdp/config.ts
+++ trigger/chains/evm/base/cdp/config.ts
@@ -17,30 +17,30 @@ export const baseCdpConfig: ChainSyncConfig = {
   limit: 40_000, // NOTE(shafu): 20_000 is not enough
   facilitators: [
     {
-      id: "coinbase",
-      syncStartDate: new Date('2025-05-05'),
-      enabled: true,
-      address: "0xdbdf3d8ed80f84c35d01c6c9f9271761bad90ba6",
-      token: USDC_BASE_TOKEN,
+        id: "coinbase",
+        enabled: true,
+        syncStartDate: new Date('2025-05-05'),
+        address: "0xdbdf3d8ed80f84c35d01c6c9f9271761bad90ba6",
+        token: USDC_BASE_TOKEN,
     },
     {
         id: "payAI",
-        syncStartDate: new Date('2025-05-18'),
         enabled: true,
+        syncStartDate: new Date('2025-05-18'),
         address: "0xc6699d2aada6c36dfea5c248dd70f9cb0235cb63",
         token: USDC_BASE_TOKEN,
     },
     {
         id: "aurracloud",
-        syncStartDate: new Date('2025-10-05'),
         enabled: true,
+        syncStartDate: new Date('2025-10-05'),
         address: "0x222c4367a2950f3b53af260e111fc3060b0983ff",
         token: USDC_BASE_TOKEN,
     },
     {
         id: "thirdweb",
-        syncStartDate: new Date('2025-10-07'),
         enabled: true,
+        syncStartDate: new Date('2025-10-07'),
         address: "0x80c08de1a05df2bd633cf520754e40fde3c794d3",
         token: USDC_BASE_TOKEN,
     }
