70538c187a21d4b83fc33fc1afa7c89921b0b715
shafu <shafu0x@gmail.com>
2025-10-25 10:22:55 -0400

    update base every hour not every minute

--- trigger/chains/evm/base/cdp/config.ts
+++ trigger/chains/evm/base/cdp/config.ts
@@ -9,7 +9,7 @@ import { buildQuery, transformResponse } from './query';
 import { FACILITATORS } from '@/trigger/config';
 
 export const baseCdpConfig: SyncConfig = {
-  cron: '*/1 * * * *',
+  cron: '0 0 * * *',
   maxDurationInSeconds: ONE_MINUTE_IN_SECONDS * 10,
   chain: 'base',
   provider: QueryProvider.CDP,
